a3fe637cc1dd9f853d4b4ee47fef7797e7606ebc
aldotestino <aldo.testino@libero.it>
2022-07-18 20:21:59 +0200

    fix shadow, github link

--- controllers/messageController.ts
+++ controllers/messageController.ts
@@ -41,15 +41,23 @@ export function messageController() {
           <script src="https://cdn.tailwindcss.com"></script>
         </head>
         <body class="bg-gray-900 text-white min-h-screen overflow-hidden">
-          <div class="w-full max-w-2xl mx-auto">
-            <header class="sticky top-0 w-full bg-gray-900 p-2 shadow-md">
+          <header class="sticky top-0 w-full bg-gray-900 shadow-md">
+            <div class="w-full max-w-2xl p-2 flex justify-between mx-auto">
+              <div></div>  
               <h1 class="text-2xl text-center">Bun Messenger</h1>
-            </header>
-            <main style="height: calc(100vh - 112px)" class="space-y-2 px-2 overflow-auto">
+              <a href="https://github.com/aldotestino/bunmessenger" target="_blank">
+                <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true">
+                  <path fill="white" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+              </a>
+            </div>
+          </header>
+          <div class="w-full max-w-2xl mx-auto">
+            <main style="height: calc(100vh - 112px)" class="space-y-2 p-2 overflow-auto">
               ${messages.map(m => messageElement(m)).join().replaceAll(",", "")}
             </main>
-            <footer class="sticky bottom-0 w-full">
-              <form accept-charset="utf-8" class="p-2 bg-gray-900 flex space-x-2 mb-0" action="/message" method="post">
+            <footer class="sticky bg-gray-900 p-2 bottom-0 w-full">
+              <form accept-charset="utf-8" class="flex space-x-2 mb-0" action="/message" method="post">
                <input required type="text" name="author" placeholder="Author" class="w-2/5 transition focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none bg-transparent py-2 px-4 text-lg placeholder-gray-500 shadow-md border-2 border-gray-500 rounded-lg"">
                <input required type="text" name="body" placeholder="Message" class="w-3/5 transition focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none bg-transparent py-2 px-4 text-lg placeholder-gray-500 shadow-md border-2 border-gray-500 rounded-lg">
                <button type="submit" class="w-full md:space-x-2 flex items-center justify-center text-gray-50 disabled:opacity-50 disabled:cursor-not-allowed transition whitespace-nowrap py-2 px-4 text-lg bg-indigo-500 rounded-lg w-auto hover:bg-indigo-600 focus:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-gray-900 shadow-md focus:ring-offset-2 focus:ring-indigo-400">
